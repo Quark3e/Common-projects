65d4719c83880287ab9ff36ea0f5a099e9d2f1b9
Quark3e <b.erkhme@gmail.com>
2020-10-05 23:19:36 +0200

    progress

--- Ignition/Ignition.ino
+++ Ignition/Ignition.ino
@@ -58,6 +58,7 @@ void loop()
         //Serial.print("Sensor Value: ");
         //Serial.println(analogRead(sound));
         //Serial.println(digitalRead(buttonPin));
+        delay(500);
   
         digitalWrite(led1, HIGH);
 
