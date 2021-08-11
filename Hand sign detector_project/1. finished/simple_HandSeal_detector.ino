96ba21bb610bc9b141d6eba2c4f8659f09b5b468
Quark3e <b.erkhme@gmail.com>
2021-08-12 01:37:32 +0200

    finish

--- Hand sign detector_project/1. finished/simple_HandSeal_detector.ino
+++ Hand sign detector_project/1. finished/simple_HandSeal_detector.ino
@@ -21,22 +21,16 @@ int Lb_7 = 8;
 int Lb_8 = 9;
 
 int startButton = 10;
-int signalingLED = 11;
-int resetLED = 12;
-int testLED = 13;
-int ChidoriLED = A0;
-int FireballLED = A1;
-int ReppushoLED = A2;
+int signalLED = 11;
 
 int breakVal = 0;
+int loopCount;
 
 byte contactReader_function();
 void arrayCreator_function(byte byteVar, int i);
 bool Chidori_compareFunc();
 bool Fireball_compareFunc();
-bool Reppusho_compareFunc();
-
-bool boolFunction(String ArrayVal, int j);
+bool Reppusho_compareFunc();  
 
 void FireBall_Jutsu();
 void LightningCutter_Jutsu();
@@ -56,78 +50,75 @@ void setup () {
     pinMode(Lb_7, INPUT_PULLUP);
     pinMode(Lb_8, INPUT_PULLUP);
     pinMode(startButton, INPUT_PULLUP);
-    pinMode(signalingLED, OUTPUT);
-    pinMode(resetLED, OUTPUT);
-    pinMode(testLED, OUTPUT);
-    pinMode(ChidoriLED, OUTPUT);
-    pinMode(FireballLED, OUTPUT);
-    pinMode(ReppushoLED, OUTPUT);
+    pinMode(signalLED, OUTPUT);
 }
 
 void loop () {
-    digitalWrite(signalingLED, LOW);
-    digitalWrite(resetLED, LOW);
-    digitalWrite(testLED, LOW);
-    digitalWrite(ChidoriLED, LOW);
-    digitalWrite(FireballLED, LOW);
-    digitalWrite(ReppushoLED, LOW);
-
     bool checker = true;
     byte checker_compare = 1;
+    breakVal = 0;
+    int j;
+
+    digitalWrite(signalLED, LOW);
 
     while (startButton) {digitalRead(startButton);}
-    digitalWrite(signalingLED, HIGH);
     delay(1000);
-    digitalWrite(signalingLED, LOW);
 
-    for (int i=0; i<8; i++) {
+    for (loopCount=0; loopCount<8; loopCount++) {
+        j = 0;
         while (checker) {
             if (contactReader_function() >= checker_compare) {checker = false;}
+            WaitingText(j);
+            delay(500);
+            j = j + 1;
+            if (breakVal) {break;}
+            delay(500);
         }
-        digitalWrite(signalingLED, HIGH);
-        arrayCreator_function(contactReader_function, i);
+        if (breakVal) {break; Serial.println("restart..");}
+        digitalWrite(signalLED, HIGH);
+        arrayCreator_function(contactReader_function(), loopCount);
+        delay(100);
         Serial.println("---------");
-        Serial.print("Array element: ");
-        Serial.print('i');
-        Serial.println(";");
+        Serial.print("Array element: #");
+        Serial.print(loopCount);
+        Serial.println(" placed.");
         Serial.println("---------");
-        if (i == 2) {
+        if (loopCount == 2) {
             if (Chidori_compareFunc()) {LightningCutter_Jutsu();}
         }
-        if (breakVal) {break;}
-        if (i == 3) {
+        if (loopCount == 3) {
             if (Fireball_compareFunc()) {FireBall_Jutsu();}
         }
-        if (breakVal) {break;}
-        if (i == 4) {
+        if (loopCount == 4) {
             if (Reppusho_compareFunc()) {GalePalm();}
         }
-        if (breakVal) {break;}
-        delay(500);
+
+        if (breakVal) {break; Serial.println("restart..");}
+        delay(2000);
     }
     Serial.println("Restarted");
-    Serial.println("------------");
+    Serial.println("---------");
 }
 
 byte contactReader_function() {
     byte returnValue = 0;
-    int val1 != digitalRead(Lb_1);
-    int val2 != digitalRead(Lb_2);
-    int val3 != digitalRead(Lb_3);
-    int val4 != digitalRead(Lb_4);
-    int val5 != digitalRead(Lb_5);
-    int val6 != digitalRead(Lb_6);
-    int val7 != digitalRead(Lb_7);
-    int val8 != digitalRead(Lb_8);
+    int val1 = digitalRead(Lb_1);
+    int val2 = digitalRead(Lb_2);
+    int val3 = digitalRead(Lb_3);
+    int val4 = digitalRead(Lb_4);
+    int val5 = digitalRead(Lb_5);
+    int val6 = digitalRead(Lb_6);
+    int val7 = digitalRead(Lb_7);
+    int val8 = digitalRead(Lb_8);
 
-    if (val1) {returnValue = returnValue | 1}
-    if (val2) {returnValue = returnValue | (1<<2);}
-    if (val3) {returnValue = returnValue | (1<<3);}
-    if (val4) {returnValue = returnValue | (1<<4);}
-    if (val5) {returnValue = returnValue | (1<<5);}
-    if (val6) {returnValue = returnValue | (1<<6);}
-    if (val7) {returnValue = returnValue | (1<<7);}
-    if (val8) {returnValue = returnValue | (1<<8);}
+    if (val1 != 1) {returnValue = returnValue | 1;}
+    if (val2 != 1) {returnValue = returnValue | (1<<2);}
+    if (val3 != 1) {returnValue = returnValue | (1<<3);}
+    if (val4 != 1) {returnValue = returnValue | (1<<4);}
+    if (val5 != 1) {returnValue = returnValue | (1<<5);}
+    if (val6 != 1) {returnValue = returnValue | (1<<6);}
+    if (val7 != 1) {returnValue = returnValue | (1<<7);}
+    if (val8 != 1) {returnValue = returnValue | (1<<8);}
 
     return returnValue;
 }
@@ -182,6 +173,9 @@ void arrayCreator_function(byte byteVar, int i) {
             Serial.println("Tiger");
             jutsu[i]= "Tiger";
             break;
+        default:
+            loopCount = loopCount - 1;
+            Serial.println("Didn't connect. for loop re-looped");
     }
 }
 
@@ -212,52 +206,41 @@ bool Reppusho_compareFunc() {
 
 void LightningCutter_Jutsu() {
     Serial.println("Raiton: Chidori");
-    digitalWrite(ChidoriLED, HIGH);
 
     delay(1000);
     breakVal = 1;
 }
 void FireBall_Jutsu() {
     Serial.println("Katon: Goukakyuu no Jutsu");
-    digitalWrite(FireballLED, HIGH);
 
     delay(1000);
     breakVal = 1;
 }
 void GalePalm() {
     Serial.println("Futon: Reppusho");
-    digitalWrite(ReppushoLED, HIGH);
 
     delay(1000);
     breakVal = 1;
 }
 
+void WaitingText (int j) {
+    Serial.print("waiting. ");
+    if (j>1 && j<3) {Serial.print(". ");}
+    if (j>3 && j<5) {Serial.print(". ");}
+    if (j>5 && j<7) {Serial.println(". "); Serial.println(" ");}
+
+    if (j>9 && j<11) {Serial.print("waiting. ");}
+    if (j>11 && j<13) {Serial.print(". ");}
+    if (j>13 && j<15) {Serial.print(". ");}
+    if (j>15 && j<17) {Serial.println(". "); Serial.println(" ");}
+}
+
 void TestLEDActivation () {
-    digitalWrite(testLED, HIGH);
-    delay(100);
-    digitalWrite(testLED, LOW);
-    delay(100);
-    digitalWrite(testLED, HIGH);
-    delay(100);
-    digitalWrite(testLED, LOW);
-    delay(100);
-    digitalWrite(testLED, HIGH);
-    delay(1000);
+
 }
 
 void resetLED_activation () {
-    delayMicroseconds(1000);
-    digitalWrite(resetLED, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(resetLED, LOW);
-    delayMicroseconds(1000);
-    digitalWrite(resetLED, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(resetLED, LOW);
-    delayMicroseconds(1000);
-    digitalWrite(resetLED, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(resetLED, LOW);
+
 }
 
 //letters used: i, j, k, n, 
